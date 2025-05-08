a65fa5543c2c5399ac8d56c55f21d39b9379ef51
dsad233 <ksl1421@naver.com>
2025-05-08 23:29:06 +0900

    feat: httpexception middleware 추가

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from './middlewares/http-exception.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalFilters(new HttpExceptionFilter());
   await app.listen(configService.getOrThrow<number>('port') ?? 3000);
   console.log(
     `현재 애플리케이션 실행 환경: ${configService.getOrThrow<number>('nodeEnv')}, ${configService.getOrThrow<number>('port')}`,
