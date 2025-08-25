b8f5b540133437692aad412c3b6dbb0c9225c20f
dsad233 <ksl1421@naver.com>
2025-08-25 19:48:32 +0900

    refactor: redis prefix로 인한 로직 수정

--- libs/enums/src/types.ts
+++ libs/enums/src/types.ts
@@ -1,4 +1,15 @@
+// 토큰 타입
 export enum TokenType {
   ACCESS = 'ACCESS',
   REFRESH = 'REFRESH',
+}
+
+// Redis Prefix
+export enum PrefixType {
+  // token session
+  SESSION = 'SESSION',
+  // product_findOne Method
+  PRODUCT = 'PRODUCT',
+  // product_find Method
+  PRODUCTS = 'PRODUCTS',
 }
